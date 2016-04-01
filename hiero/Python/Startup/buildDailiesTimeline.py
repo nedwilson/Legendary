45fac549468b25f8b6775f8f99ae7fcc76727b6a
Ned Wilson <nedwilson@gmail.com>
2016-04-01 09:35:51 -0700

    Disable addition of EXR plate, EXR comp, and CC layer for temp shots

--- hiero/Python/Startup/buildDailiesTimeline.py
+++ hiero/Python/Startup/buildDailiesTimeline.py
@@ -73,7 +73,9 @@ class BuildDailiesTimeline(QAction):
 		tl_bin.addItem(review_seq_bi)
 
 		vt_pos = 0
+		b_is_temp = False
 		for qt_file in sorted(glob.glob(os.path.join(g_s_dailies_dir, '_vfx', '*.mov'))):
+			b_is_temp = False
 			tmp_basename = os.path.basename(qt_file).replace('_vfx', '').split('.')[0]
 			tmp_shot = os.path.basename(qt_file).split('_')[0]
 			tmp_seq = tmp_shot[0:3]
@@ -89,52 +91,56 @@ class BuildDailiesTimeline(QAction):
 			tmp_qt_track_item.setTimelineOut(vt_pos + tmp_qt_ms.duration() - 2)
 			tmp_qt_track_item.reformatState().setType("disabled")
 			qt_video_track.addItem(tmp_qt_track_item)
+			
+			if "temp" in qt_file:
+				b_is_temp = True
+				
+			if not b_is_temp:
+				# Create EXR Media Source
+				tmp_exr_file = os.path.join(self.g_s_project_root, tmp_seq, tmp_shot, 'pix', 'comp', tmp_basename, "%s.%%04d.exr"%tmp_basename)
+				tmp_exr_ms = hiero.core.MediaSource(tmp_exr_file)
+				tmp_exr_clip = hiero.core.Clip(tmp_exr_ms, (tmp_exr_ms.startTime() + 1), (tmp_exr_ms.startTime() + tmp_exr_ms.duration() - 1))
+				exr_bin.addItem(hiero.core.BinItem(tmp_exr_clip))
+				# Create EXR TrackItem
+				tmp_exr_track_item = exr_video_track.createTrackItem(tmp_shot)
+				tmp_exr_track_item.setSource(tmp_exr_clip)
+				tmp_exr_track_item.setTimelineIn(vt_pos)
+				tmp_exr_track_item.setTimelineOut(vt_pos + tmp_exr_ms.duration() - 2)
+				tmp_exr_track_item.reformatState().setType("disabled")
+				exr_video_track.addItem(tmp_exr_track_item)
 
-			# Create EXR Media Source
-			tmp_exr_file = os.path.join(self.g_s_project_root, tmp_seq, tmp_shot, 'pix', 'comp', tmp_basename, "%s.%%04d.exr"%tmp_basename)
-			tmp_exr_ms = hiero.core.MediaSource(tmp_exr_file)
-			tmp_exr_clip = hiero.core.Clip(tmp_exr_ms, (tmp_exr_ms.startTime() + 1), (tmp_exr_ms.startTime() + tmp_exr_ms.duration() - 1))
-			exr_bin.addItem(hiero.core.BinItem(tmp_exr_clip))
-			# Create EXR TrackItem
-			tmp_exr_track_item = exr_video_track.createTrackItem(tmp_shot)
-			tmp_exr_track_item.setSource(tmp_exr_clip)
-			tmp_exr_track_item.setTimelineIn(vt_pos)
-			tmp_exr_track_item.setTimelineOut(vt_pos + tmp_exr_ms.duration() - 2)
-			tmp_exr_track_item.reformatState().setType("disabled")
-			exr_video_track.addItem(tmp_exr_track_item)
+				# Create EXR Plate Media Source
+				plate_tmp_dir = self.likely_plate(tmp_basename)
+				tmp_exr_plate_file = os.path.join(plate_tmp_dir, "%s.%%04d.exr"%os.path.basename(plate_tmp_dir))
+				tmp_exr_plate_ms = hiero.core.MediaSource(tmp_exr_plate_file)
+				tmp_exr_plate_clip = hiero.core.Clip(tmp_exr_plate_ms, (tmp_exr_ms.startTime() + 1), (tmp_exr_ms.startTime() + tmp_exr_ms.duration() - 1))
+				plates_bin.addItem(hiero.core.BinItem(tmp_exr_plate_clip))
+				# Create EXR Plate TrackItem
+				tmp_exr_plate_track_item = plate_video_track.createTrackItem(tmp_shot)
+				tmp_exr_plate_track_item.setSource(tmp_exr_plate_clip)
+				tmp_exr_plate_track_item.setTimelineIn(vt_pos)
+				tmp_exr_plate_track_item.setTimelineOut(vt_pos + tmp_exr_ms.duration() - 2)
+				tmp_exr_plate_track_item.reformatState().setType("disabled")
+				plate_video_track.addItem(tmp_exr_plate_track_item)
 
-			# Create EXR Plate Media Source
-			plate_tmp_dir = self.likely_plate(tmp_basename)
-			tmp_exr_plate_file = os.path.join(plate_tmp_dir, "%s.%%04d.exr"%os.path.basename(plate_tmp_dir))
-			tmp_exr_plate_ms = hiero.core.MediaSource(tmp_exr_plate_file)
-			tmp_exr_plate_clip = hiero.core.Clip(tmp_exr_plate_ms, (tmp_exr_ms.startTime() + 1), (tmp_exr_ms.startTime() + tmp_exr_ms.duration() - 1))
-			plates_bin.addItem(hiero.core.BinItem(tmp_exr_plate_clip))
-			# Create EXR Plate TrackItem
-			tmp_exr_plate_track_item = plate_video_track.createTrackItem(tmp_shot)
-			tmp_exr_plate_track_item.setSource(tmp_exr_plate_clip)
-			tmp_exr_plate_track_item.setTimelineIn(vt_pos)
-			tmp_exr_plate_track_item.setTimelineOut(vt_pos + tmp_exr_ms.duration() - 2)
-			tmp_exr_plate_track_item.reformatState().setType("disabled")
-			plate_video_track.addItem(tmp_exr_plate_track_item)
-
-			# Add Color Correction Layer
-			cs_effect = hiero.core.EffectTrackItem('OCIOColorSpace', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
-			cs_effect.node().knob('out_colorspace').setValue('AlexaV3LogC')
-			cc_video_track.addSubTrackItem(cs_effect, 0)
+				# Add Color Correction Layer
+				cs_effect = hiero.core.EffectTrackItem('OCIOColorSpace', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
+				cs_effect.node().knob('out_colorspace').setValue('AlexaV3LogC')
+				cc_video_track.addSubTrackItem(cs_effect, 0)
 	
-			tmp_cdl_file = os.path.join(self.g_s_project_root, tmp_seq, tmp_shot, 'data', 'cdl', '%s.cdl'%tmp_shot)
-			cdl_effect = hiero.core.EffectTrackItem('OCIOCDLTransform', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
-			cdl_effect.node().knob('read_from_file').setValue(True)
-			cdl_effect.node().knob('file').setValue(tmp_cdl_file)
-			cc_video_track.addSubTrackItem(cdl_effect, 1)
+				tmp_cdl_file = os.path.join(self.g_s_project_root, tmp_seq, tmp_shot, 'data', 'cdl', '%s.cdl'%tmp_shot)
+				cdl_effect = hiero.core.EffectTrackItem('OCIOCDLTransform', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
+				cdl_effect.node().knob('read_from_file').setValue(True)
+				cdl_effect.node().knob('file').setValue(tmp_cdl_file)
+				cc_video_track.addSubTrackItem(cdl_effect, 1)
 
-			lut_effect = hiero.core.EffectTrackItem('OCIOFileTransform', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
-			lut_effect.node().knob('file').setValue(self.g_s_show_lut)
-			cc_video_track.addSubTrackItem(lut_effect, 2)
+				lut_effect = hiero.core.EffectTrackItem('OCIOFileTransform', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
+				lut_effect.node().knob('file').setValue(self.g_s_show_lut)
+				cc_video_track.addSubTrackItem(lut_effect, 2)
 	
-			cs_effect_two = hiero.core.EffectTrackItem('OCIOColorSpace', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
-			cs_effect_two.node().knob('in_colorspace').setValue('rec709')
-			cc_video_track.addSubTrackItem(cs_effect_two, 3)
+				cs_effect_two = hiero.core.EffectTrackItem('OCIOColorSpace', timelineIn=vt_pos, timelineOut=(vt_pos + tmp_exr_ms.duration() - 2))
+				cs_effect_two.node().knob('in_colorspace').setValue('rec709')
+				cc_video_track.addSubTrackItem(cs_effect_two, 3)
 
 			vt_pos += tmp_qt_ms.duration() - 1
 	
