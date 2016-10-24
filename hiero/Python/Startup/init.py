3713cdb74a8e6a4a74d80dbe472d8ce854e00ba6
Ned Wilson <nedwilson@gmail.com>
2016-10-24 14:43:13 -0700

    Remove hiero sub directory from Legendary Repository.

--- hiero/Python/Startup/init.py
+++
@@ -1,8 +0,0 @@
-import hiero.core
-hiero.core.log.setLogLevel(hiero.core.log.kDebug)
-import hiero.ui
-import addCCVideoTrack
-import buildDailiesTimeline
-import buildShotLayers
-import FnLinkExporter
-import FnLinkExporterUI
